e7e03e5ec309dfdb714bcd133811f8e1b917a36a
Emmanuel Hernandez <calcetin.izquierdo@gmail.com>
2024-01-08 13:15:51 -0600

    update App component to add a random animal, and then show it on the screen whenever the user clicks the button

--- animals/src/App.js
+++ animals/src/App.js
@@ -1,12 +1,25 @@
 import { useState } from 'react';
+import AnimalShow from './AnimalShow';
+
+function getRandomAnimal() {
+  const animals = ['bird', 'cat', 'cow', 'dog', 'gator', 'horse'];
+
+  return animals[Math.floor(Math.random() * animals.length)];
+}
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [animals, setAnimals] = useState([]);
+
+  const handleClick = () => setAnimals([...animals, getRandomAnimal()]);
+
+  const renderedAnimals = animals.map((animal, index) => {
+    return <AnimalShow type={animal} key={index} />;
+  });
 
   return (
     <div>
-      <button onClick={() => setCount(count + 1)}>Add Animal </button>
-      <div>Number of animals: {count}</div>
+      <button onClick={handleClick}>Add Animal</button>
+      <div>{renderedAnimals}</div>
     </div>
   );
 }
