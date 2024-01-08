d79c47c3d531f11b1de11c282ddb31237f519867
Emmanuel Hernandez <calcetin.izquierdo@gmail.com>
2024-01-08 13:49:43 -0600

    apply styles to AnimalShow component

--- animals/src/AnimalShow.js
+++ animals/src/AnimalShow.js
@@ -1,3 +1,4 @@
+import './AnimalShow.css';
 import { useState } from 'react';
 import bird from './svg/bird.svg';
 import cat from './svg/cat.svg';
@@ -15,9 +16,14 @@ function AnimalShow({ type }) {
   const handleClick = () => setClicks(clicks + 1);
 
   return (
-    <div onClick={handleClick}>
-      <img alt={type} src={svgMap[type]} />
-      <img alt="heart" src={heart} style={{ width: 10 + 10 * clicks + 'px' }} />
+    <div className="animal-show" onClick={handleClick}>
+      <img className="animal" alt={type} src={svgMap[type]} />
+      <img
+        className="heart"
+        alt="heart"
+        src={heart}
+        style={{ width: 10 + 10 * clicks + 'px' }}
+      />
     </div>
   );
 }
