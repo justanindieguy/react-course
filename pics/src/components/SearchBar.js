75dcc68939bd1e838129d744596ba08e506e8a8a
Emmanuel Hernandez <calcetin.izquierdo@gmail.com>
2024-01-10 11:12:23 -0600

    update SearchBar component to handle changes in input element and call onSubmit when the user presses the enter key

--- pics/src/components/SearchBar.js
+++ pics/src/components/SearchBar.js
@@ -1,5 +1,22 @@
-function SearchBar() {
-  return <div>SearchBar</div>;
+import { useState } from 'react';
+
+function SearchBar({ onSubmit }) {
+  const [term, setTerm] = useState('');
+
+  const handleFormSubmit = (evt) => {
+    evt.preventDefault();
+    onSubmit(term);
+  };
+
+  const handleChange = (evt) => setTerm(evt.target.value);
+
+  return (
+    <div>
+      <form onSubmit={handleFormSubmit}>
+        <input value={term} onChange={handleChange} />
+      </form>
+    </div>
+  );
 }
 
 export default SearchBar;
