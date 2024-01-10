0c1acfa7b0fa6b18ae730457c95e867689688bd0
Emmanuel Hernandez <calcetin.izquierdo@gmail.com>
2024-01-10 13:31:50 -0600

    update App component to update state with the response from unsplash api and then pass it to ImageList component as a prop

--- pics/src/App.js
+++ pics/src/App.js
@@ -1,15 +1,21 @@
+import { useState } from 'react';
 import SearchBar from './components/SearchBar';
+import ImageList from './components/ImageList';
 import searchImages from './api';
 
 function App() {
+  const [images, setImages] = useState([]);
+
   const handleSubmit = async (term) => {
-    const images = await searchImages(term);
-    console.log(images);
+    const response = await searchImages(term);
+
+    setImages(response);
   };
 
   return (
     <div>
       <SearchBar onSubmit={handleSubmit} />
+      <ImageList images={images} />
     </div>
   );
 }
