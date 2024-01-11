416200b79c173fac776051bcdb71611a026f68d8
Emmanuel Hernandez <calcetin.izquierdo@gmail.com>
2024-01-11 12:16:50 -0600

    write createBook function to update books state array, pass this function as a prop to BookCreate component

--- books/src/App.js
+++ books/src/App.js
@@ -1,5 +1,18 @@
+import { useState } from 'react';
+import BookCreate from './components/BookCreate';
+
 function App() {
-  return <div>App</div>;
+  const [books, setBooks] = useState([]);
+
+  const createBook = (title) =>
+    setBooks([...books, { id: Math.floor(Math.random() * 9999), title }]);
+
+  return (
+    <div>
+      {books.length}
+      <BookCreate onCreate={createBook} />
+    </div>
+  );
 }
 
 export default App;
