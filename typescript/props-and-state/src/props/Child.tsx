bb73df045271d61b7b84695abd23935003671e07
Emmanuel Hernandez <calcetin.izquierdo@gmail.com>
2024-01-18 13:15:25 -0600

    edit ChildProps to receive children from Parent

--- typescript/props-and-state/src/props/Child.tsx
+++ typescript/props-and-state/src/props/Child.tsx
@@ -1,16 +1,34 @@
 interface ChildProps {
+  children?: React.ReactNode;
   color: string;
+  handleClick: () => void;
 }
 
 // Easiest way to indicate props and their types
-export function Child({ color }: ChildProps) {
-  return <div>{color}</div>;
+export function Child({ children, color, handleClick }: ChildProps) {
+  return (
+    <div>
+      <div>{children}</div>
+      <div>{color}</div>
+      <button onClick={handleClick}>Click Me!</button>
+    </div>
+  );
 }
 
 /**
  * Allows to use other properties associated with Function Components such as
  * display name
  */
-export const ChildAsFC: React.FC<ChildProps> = ({ color }) => {
-  return <div>{color}</div>;
+export const ChildAsFC: React.FC<ChildProps> = ({
+  children,
+  color,
+  handleClick,
+}) => {
+  return (
+    <div>
+      <div>{children}</div>
+      <div>{color}</div>
+      <button onClick={handleClick}>Click Me!</button>
+    </div>
+  );
 };
