6662629c7140d632dc34f5dfb3d7bdefbfefcb91
Emmanuel Hernandez <calcetin.izquierdo@gmail.com>
2024-01-18 13:00:37 -0600

    learn about React.FunctionComponent interface

--- typescript/props-and-state/src/props/Child.tsx
+++ typescript/props-and-state/src/props/Child.tsx
@@ -2,6 +2,15 @@ interface ChildProps {
   color: string;
 }
 
+// Easiest way to indicate props and their types
 export function Child({ color }: ChildProps) {
   return <div>{color}</div>;
 }
+
+/**
+ * Allows to use other properties associated with Function Components such as
+ * display name
+ */
+export const ChildAsFC: React.FC<ChildProps> = ({ color }) => {
+  return <div>{color}</div>;
+};
