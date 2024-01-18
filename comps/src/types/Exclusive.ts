8aaceb66543db3059f661ad6987c8224ceea6246
Emmanuel Hernandez <calcetin.izquierdo@gmail.com>
2024-01-18 16:57:28 -0600

    create type Exclusive

---
+++ comps/src/types/Exclusive.ts
@@ -0,0 +1,9 @@
+import ExcludeFromTuple from './ExcludeFromTuple';
+
+type Exclusive<T extends PropertyKey[], U = any> = T[number] extends infer E
+  ? E extends string
+    ? Record<E, U> & { [k in ExcludeFromTuple<T, E>]?: never }
+    : never
+  : never & {};
+
+export default Exclusive;
