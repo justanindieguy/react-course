e3d24cca8ab767845ba666e5696d769f5b01bdaf
Emmanuel Hernandez <calcetin.izquierdo@gmail.com>
2024-01-19 09:43:56 -0600

    add Button styles for hover and active states

--- comps/src/Button.tsx
+++ comps/src/Button.tsx
@@ -2,9 +2,11 @@ import className from 'classnames';
 import { twMerge } from 'tailwind-merge';
 import Exclusive from './types/Exclusive';
 
+type Variants = ['primary', 'secondary', 'success', 'warning', 'danger'];
+
 type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement> &
   Partial<{ outline?: boolean; rounded?: boolean }> &
-  Exclusive<['primary', 'secondary', 'success', 'warning', 'danger']>;
+  Exclusive<Variants>;
 
 const Button: React.FC<ButtonProps> = ({
   children,
@@ -19,13 +21,18 @@ const Button: React.FC<ButtonProps> = ({
 }) => {
   const classes = twMerge(
     className(rest.className, 'flex items-center px-3 py-1.5 border', {
-      'border-blue-500 bg-blue-500 text-white': primary,
-      'border-gray-900 bg-gray-900 text-white': secondary,
-      'border-green-500 bg-green-500 text-white': success,
-      'border-yellow-400 bg-yellow-400 text-white': warning,
-      'border-red-500 bg-red-500 text-white': danger,
+      'border-blue-500 bg-blue-500 hover:bg-blue-600 active:bg-blue-700 text-white':
+        primary,
+      'border-gray-900 bg-gray-900 hover:bg-gray-800 active:bg-gray-700 text-white':
+        secondary,
+      'border-green-500 bg-green-500 hover:bg-green-600 active:bg-green-700 text-white':
+        success,
+      'border-yellow-400 bg-yellow-400 hover:bg-yellow-500 active:bg-yellow-600 text-white':
+        warning,
+      'border-red-500 bg-red-500 hover:bg-red-600 active:bg-red-700 text-white':
+        danger,
       'rounded-full': rounded,
-      'bg-white': outline,
+      'bg-white hover:bg-gray-100 active:bg-gray-200': outline,
       'text-blue-500': outline && primary,
       'text-gray-900': outline && secondary,
       'text-green-500': outline && success,
