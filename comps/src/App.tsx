514364c824ac4e64bf325807896ea89b558f4aa9
Emmanuel Hernandez <calcetin.izquierdo@gmail.com>
2024-01-23 09:30:52 -0600

    add list of options for Dropdown component and render it

--- comps/src/App.tsx
+++ comps/src/App.tsx
@@ -1,5 +1,23 @@
+import { useState } from 'react';
+import Dropdown, { DropdownOption } from './components/Dropdown';
+
 const App: React.FC = () => {
-  return <h1>Hello, World!</h1>;
+  const [selection, setSelection] = useState<DropdownOption | null>(null);
+
+  const handleSelect = (option: DropdownOption) => setSelection(option);
+
+  const options = [
+    { label: 'Red', value: 'red' },
+    { label: 'Green', value: 'green' },
+    { label: 'Blue', value: 'blue' },
+  ];
+
+  return (
+    <div className="flex">
+      <Dropdown options={options} value={selection} onChange={handleSelect} />
+      <Dropdown options={options} value={selection} onChange={handleSelect} />
+    </div>
+  );
 };
 
 export default App;
