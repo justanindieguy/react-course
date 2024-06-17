10c7bc14e66a04c515b03613e64446c0ec25a544
Emmanuel Hernandez <calcetin.izquierdo@gmail.com>
2024-06-16 19:24:17 -0600

    move creation of dropdown to its own page component

--- comps/src/App.tsx
+++ comps/src/App.tsx
@@ -1,21 +1,9 @@
-import { useState } from 'react';
-import Dropdown, { DropdownOption } from './components/Dropdown';
+import DropdownPage from './pages/DropdownPage';
 
 const App: React.FC = () => {
-  const [selection, setSelection] = useState<DropdownOption | null>(null);
-
-  const handleSelect = (option: DropdownOption) => setSelection(option);
-
-  const options = [
-    { label: 'Red', value: 'red' },
-    { label: 'Green', value: 'green' },
-    { label: 'Blue', value: 'blue' },
-  ];
-
   return (
-    <div className="flex">
-      <Dropdown options={options} value={selection} onChange={handleSelect} />
-      <Dropdown options={options} value={selection} onChange={handleSelect} />
+    <div>
+      <DropdownPage />
     </div>
   );
 };
