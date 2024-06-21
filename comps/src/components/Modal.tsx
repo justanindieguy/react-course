99dee8dc37a24e6b2cd9df5948a7366a42ae6910
Emmanuel Hernandez <calcetin.izquierdo@gmail.com>
2024-06-21 17:48:44 -0600

    add new props to render content inside modal: children and actionBar

--- comps/src/components/Modal.tsx
+++ comps/src/components/Modal.tsx
@@ -1,17 +1,25 @@
 import ReactDOM from 'react-dom';
 
-interface ModalProps {
+type ModalProps = React.HTMLAttributes<HTMLElement> & {
+  actionBar: JSX.Element;
   onClose: () => void;
-}
+};
 
-const Modal = ({ onClose }: ModalProps): React.ReactPortal => {
+const Modal = ({
+  actionBar,
+  children,
+  onClose,
+}: ModalProps): React.ReactPortal => {
   return ReactDOM.createPortal(
     <div>
       <div
         onClick={onClose}
         className="absolute inset-0 bg-gray-300 opacity-80"
       ></div>
-      <div className="absolute inset-40 p-10 bg-white">I'm a modal!</div>
+      <div className="absolute inset-40 p-10 bg-white">
+        {children}
+        {actionBar}
+      </div>
     </div>,
     document.querySelector('.modal-container')!,
   );
