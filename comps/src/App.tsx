94a86b2acae8908977a8379253a357fc7f5d58a1
Emmanuel Hernandez <calcetin.izquierdo@gmail.com>
2024-06-21 13:20:52 -0600

    render sidebar component with its content to the right side

--- comps/src/App.tsx
+++ comps/src/App.tsx
@@ -1,9 +1,24 @@
+import Sidebar from './components/Sidebar';
+import Route from './components/Route';
 import DropdownPage from './pages/DropdownPage';
+import AccordionPage from './pages/AccordionPage';
+import ButtonPage from './pages/ButtonPage';
 
 const App: React.FC = () => {
   return (
-    <div>
-      <DropdownPage />
+    <div className="container mx-auto grid grid-cols-6 gap-4 mt-4">
+      <Sidebar />
+      <div className="col-span-5">
+        <Route path="/">
+          <DropdownPage />
+        </Route>
+        <Route path="/accordion">
+          <AccordionPage />
+        </Route>
+        <Route path="/buttons">
+          <ButtonPage />
+        </Route>
+      </div>
     </div>
   );
 };
