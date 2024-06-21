5654b5ed310c3f210e4e47a83da1e970e8c26258
Emmanuel Hernandez <calcetin.izquierdo@gmail.com>
2024-06-21 14:57:48 -0600

    implement functionality to open the modal when clicking a button in ModalPage component

--- comps/src/components/Modal.tsx
+++ comps/src/components/Modal.tsx
@@ -1,5 +1,13 @@
-const Modal: React.FC = () => {
-  return <div>Modal</div>;
+import ReactDOM from 'react-dom';
+
+const Modal = (): React.ReactPortal => {
+  return ReactDOM.createPortal(
+    <div>
+      <div className="absolute inset-0 bg-gray-300 opacity-80"></div>
+      <div className="absolute inset-40 p-10 bg-white">I'm a modal!</div>
+    </div>,
+    document.querySelector('.modal-container')!,
+  );
 };
 
 export default Modal;
--- comps/src/pages/ModalPage.tsx
+++ comps/src/pages/ModalPage.tsx
@@ -1,9 +1,19 @@
+import { useState } from 'react';
+
 import Modal from '../components/Modal';
+import Button from '../components/Button';
 
 const ModalPage: React.FC = () => {
+  const [showModal, setShowModal] = useState(false);
+
+  const handleClick = () => setShowModal(true);
+
   return (
     <div>
-      <Modal />
+      <Button onClick={handleClick} primary rounded>
+        Open Modal
+      </Button>
+      {showModal && <Modal />}
     </div>
   );
 };
