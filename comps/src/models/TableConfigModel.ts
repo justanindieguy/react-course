45c7307984aa979e3f319898be846836c0cdd2ba
Emmanuel Hernandez <calcetin.izquierdo@gmail.com>
2024-07-14 11:00:11 -0600

    add optional property 'sortValue' to indicate that a column can be sorted

--- comps/src/models/TableConfigModel.ts
+++ comps/src/models/TableConfigModel.ts
@@ -1,7 +1,6 @@
-import React from 'react';
-
 export interface TableConfigModel<T> {
   header?: () => React.ReactElement;
   label: string;
   render: (object: T) => React.ReactNode;
+  sortValue?: (object: T) => string | number;
 }
