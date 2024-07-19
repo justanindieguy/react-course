b3459f0f3b7814af76fb892535baa52f0db2d2b7
Emmanuel Hernandez <calcetin.izquierdo@gmail.com>
2024-07-19 14:21:51 -0600

    move counter logic into a custom hook

--- comps/src/pages/CounterPage.tsx
+++ comps/src/pages/CounterPage.tsx
@@ -1,23 +1,17 @@
-import { useEffect, useState } from 'react';
 import Button from '../components/Button';
+import useCounter from '../hooks/use-counter';
 
 interface CounterPageProps {
   initialCount: number;
 }
 
 const CounterPage: React.FC<CounterPageProps> = ({ initialCount }) => {
-  const [count, setCount] = useState(initialCount);
-
-  useEffect(() => {
-    console.log(count);
-  }, [count]);
-
-  const handleClick = () => setCount(count + 1);
+  const { count, increment } = useCounter(initialCount);
 
   return (
     <div>
       <h1>Count is {count}</h1>
-      <Button primary rounded onClick={handleClick}>
+      <Button primary rounded onClick={increment}>
         Increment
       </Button>
     </div>
