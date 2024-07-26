aa4c1317b99ed2969fa6360fafe1fc5600a26516
Emmanuel Hernandez <calcetin.izquierdo@gmail.com>
2024-07-26 12:22:23 -0600

    add a form to add custom values at once, remove custom hook and implement count logic directly in component

--- comps/src/pages/CounterPage.tsx
+++ comps/src/pages/CounterPage.tsx
@@ -1,20 +1,60 @@
+import React, { useEffect, useState } from 'react';
+
+import Panel from '../components/Panel';
 import Button from '../components/Button';
-import useCounter from '../hooks/use-counter';
 
 interface CounterPageProps {
   initialCount: number;
 }
 
 const CounterPage: React.FC<CounterPageProps> = ({ initialCount }) => {
-  const { count, increment } = useCounter(initialCount);
+  const [count, setCount] = useState(initialCount);
+  const [valueToAdd, setValueToAdd] = useState(0);
+
+  const increment = () => setCount(count + 1);
+
+  const decrement = () => setCount(count - 1);
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const value = parseInt(event.target.value) || 0;
+
+    setValueToAdd(value);
+  };
+
+  const handleSubmit = (event: React.SyntheticEvent) => {
+    event.preventDefault();
+
+    setCount(count + valueToAdd);
+    setValueToAdd(0);
+  };
+
+  useEffect(() => console.log(count), [count]);
 
   return (
-    <div>
-      <h1>Count is {count}</h1>
-      <Button primary rounded onClick={increment}>
-        Increment
-      </Button>
-    </div>
+    <Panel className="m-3">
+      <h1 className="text-lg">Count is {count}</h1>
+      <div className="flex flex-row">
+        <Button danger onClick={decrement}>
+          Decrement
+        </Button>
+        <Button success onClick={increment}>
+          Increment
+        </Button>
+      </div>
+
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="increment">Add a lot</label>
+        <input
+          value={valueToAdd || ''}
+          onChange={handleChange}
+          type="number"
+          name="increment"
+          id="increment"
+          className="p-1 m-3 bg-gray-50 border border-gray-300"
+        />
+        <Button primary>Add it!</Button>
+      </form>
+    </Panel>
   );
 };
 
