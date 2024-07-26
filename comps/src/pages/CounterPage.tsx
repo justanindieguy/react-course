d7fc8454f49a67465d6b2f07e6722a5db57c3f07
Emmanuel Hernandez <calcetin.izquierdo@gmail.com>
2024-07-26 16:20:56 -0600

    refactor CounterPage component to use useReducer hook instead of useState, learn about useReducer hook

--- comps/src/pages/CounterPage.tsx
+++ comps/src/pages/CounterPage.tsx
@@ -1,39 +1,75 @@
-import React, { useEffect, useState } from 'react';
+import React, { useReducer } from 'react';
 
 import Panel from '../components/Panel';
 import Button from '../components/Button';
+
+interface State {
+  count: number;
+  valueToAdd: number;
+}
+
+enum ActionTypes {
+  AddValueToCount = 'AddValueToCount',
+  IncrementCount = 'IncrementCount',
+  DecrementCount = 'DecrementCount',
+  SetValueToAdd = 'SetValueToAdd',
+}
+
+type Action =
+  | { type: ActionTypes.AddValueToCount }
+  | { type: ActionTypes.SetValueToAdd; payload: number }
+  | { type: ActionTypes.IncrementCount }
+  | { type: ActionTypes.DecrementCount };
+
+const reducer = (state: State, action: Action): State => {
+  switch (action.type) {
+    case ActionTypes.AddValueToCount:
+      return {
+        ...state,
+        count: state.count + state.valueToAdd,
+        valueToAdd: 0,
+      };
+    case ActionTypes.SetValueToAdd:
+      return { ...state, valueToAdd: action.payload };
+    case ActionTypes.IncrementCount:
+      return { ...state, count: state.count + 1 };
+    case ActionTypes.DecrementCount:
+      return { ...state, count: state.count - 1 };
+    default:
+      throw new Error('Unexpected action type');
+  }
+};
 
 interface CounterPageProps {
   initialCount: number;
 }
 
 const CounterPage: React.FC<CounterPageProps> = ({ initialCount }) => {
-  const [count, setCount] = useState(initialCount);
-  const [valueToAdd, setValueToAdd] = useState(0);
+  const [state, dispatch] = useReducer(reducer, {
+    count: initialCount,
+    valueToAdd: 0,
+  });
 
-  const increment = () => setCount(count + 1);
+  const increment = () => dispatch({ type: ActionTypes.IncrementCount });
 
-  const decrement = () => setCount(count - 1);
+  const decrement = () => dispatch({ type: ActionTypes.DecrementCount });
 
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const value = parseInt(event.target.value) || 0;
 
-    setValueToAdd(value);
+    dispatch({ type: ActionTypes.SetValueToAdd, payload: value });
   };
 
   const handleSubmit = (event: React.SyntheticEvent) => {
     event.preventDefault();
 
-    setCount(count + valueToAdd);
-    setValueToAdd(0);
+    dispatch({ type: ActionTypes.AddValueToCount });
   };
-
-  useEffect(() => console.log(count), [count]);
 
   return (
     <Panel className="m-3">
-      <h1 className="text-lg">Count is {count}</h1>
-      <div className="flex flex-row">
+      <h1 className="text-lg">Count is {state.count}</h1>
+      <div className="flex flex-row gap-1">
         <Button danger onClick={decrement}>
           Decrement
         </Button>
@@ -43,9 +79,9 @@ const CounterPage: React.FC<CounterPageProps> = ({ initialCount }) => {
       </div>
 
       <form onSubmit={handleSubmit}>
-        <label htmlFor="increment">Add a lot</label>
+        <label htmlFor="increment">Add a lot:</label>
         <input
-          value={valueToAdd || ''}
+          value={state.valueToAdd || ''}
           onChange={handleChange}
           type="number"
           name="increment"
